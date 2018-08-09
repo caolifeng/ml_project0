4a82e7a50682abe16825653d96e128e20bf3e3d8
caolifeng <caolifeng@192.168.31.209>
2018-08-09 09:17:38 +0800

    v0808

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+# Import libraries necessary for this project
 import numpy as np
 import pandas as pd
-
-
-
-print('HELLO WORLD!')
+from IPython.display import display # Allows the use of display() for DataFrames
+# Import supplementary visualizations code visuals.py
+import vpython as vs
+# Pretty display for notebooks
+#%matplotlib inline
+# Load the dataset
+in_file = 'titanic_data.csv'
+full_data = pd.read_csv(in_file)
+# Print the first few entries of the RMS Titanic data
+display(full_data.head())
